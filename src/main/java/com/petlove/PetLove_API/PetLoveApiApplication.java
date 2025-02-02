4291431c8a56ed98fb7d17710ec42b42a2c43178
ErickWarmling <warmling.erick@gmail.com>
2025-02-01 22:13:03 -0300

    Project created

---
+++ src/main/java/com/petlove/PetLove_API/PetLoveApiApplication.java
@@ -0,0 +1,13 @@
+package com.petlove.PetLove_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLoveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLoveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petlove/PetLove_API/PetLoveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlove.PetLove_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLoveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
